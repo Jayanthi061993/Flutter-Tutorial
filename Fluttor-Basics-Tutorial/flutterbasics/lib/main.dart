33ed0b8838eacd6323c37be375932d2e9275c946
Jayanthi C <sivaranjanijayanthi@gmail.com>
2022-12-30 23:39:13 +0530

    flutter-basics

--- Fluttor-Basics-Tutorial/flutterbasics/lib/main.dart
+++ Fluttor-Basics-Tutorial/flutterbasics/lib/main.dart
@@ -1,27 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-
-
-
-
-/**void main() {
   runApp(const Center(
     child: Text(
       'Hello World',
       textDirection: TextDirection.ltr,
     ),
   ));
-} **/
+}
