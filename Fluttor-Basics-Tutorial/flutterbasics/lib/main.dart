0133197e821eb35fcab4bdaf44894644b9be5acb
Jayanthi C <sivaranjanijayanthi@gmail.com>
2023-01-15 19:53:46 +0530

    materialapp widget

--- Fluttor-Basics-Tutorial/flutterbasics/lib/main.dart
+++ Fluttor-Basics-Tutorial/flutterbasics/lib/main.dart
@@ -6,6 +6,31 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
           [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp])
+      .then((value) => runApp(const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FirstPage',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('FirstPage'),
+          ),
+          body: const Center(child: Text('Hello World'))),
+    );
+  }
+}
+
+/**
+ * void main() {
+  //runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp])
       .then((value) => runApp(MaterialApp(
             title: 'FirstPage',
             home: Scaffold(
@@ -15,3 +40,5 @@ void main() {
                 body: const Center(child: Text('Hello'))),
           )));
 }
+ */
+
