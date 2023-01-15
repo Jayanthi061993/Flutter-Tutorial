35310759b76284b2da383dfeababc44f91d0578c
Jayanthi C <sivaranjanijayanthi@gmail.com>
2023-01-15 16:13:13 +0530

    materialapp widget

--- Fluttor-Basics-Tutorial/flutterbasics/lib/main.dart
+++ Fluttor-Basics-Tutorial/flutterbasics/lib/main.dart
@@ -5,33 +5,7 @@ void main() {
   //runApp(const MyApp());
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((value) => runApp(const MyApp()));
+      .then((value) => runApp(const MaterialApp(
+            title: 'FirstPage',
+          )));
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    int counter = 0;
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: <Widget>[
-            Text("Counter Value: => {$counter}"),
-            Checkbox(
-              value: false,
-              onChanged: (newValue) {
-                counter++;
-                // ignore: avoid_print
-                print("Counter Value: => {$counter}");
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
- //Count Value incremented on every button click but not rendered in widget
