c3d3312621ffbfec2b079e594d02f095efc4a850
Jayanthi C <sivaranjanijayanthi@gmail.com>
2023-01-15 20:35:16 +0530

    withoutMaterialApp

--- Fluttor-Basics-Tutorial/flutterbasics/lib/main.dart
+++ Fluttor-Basics-Tutorial/flutterbasics/lib/main.dart
@@ -7,67 +7,10 @@ void main() {
   SystemChrome.setPreferredOrientations(
           [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp])
       .then((value) => runApp(
-            // ignore: prefer_const_constructors
-            MaterialApp(
-              title: 'MyFirtsPage',
-              home: const FirstPage(),
-            ),
-          ));
-}
-
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('FirstPage'),
-        ),
-        body: const Center(child: Text('Hello World!!!!!!')));
-  }
-}
-
-/**
- * void main() {
-  //runApp(const MyApp());
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-          [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp])
-      .then((value) => runApp(const MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FirstPage',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('FirstPage'),
-          ),
-          body: const Center(child: Text('Hello World'))),
-    );
-  }
-}
- */
-
-/**
- * void main() {
-  //runApp(const MyApp());
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-          [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp])
-      .then((value) => runApp(MaterialApp(
-            title: 'FirstPage',
-            home: Scaffold(
+            Scaffold(
                 appBar: AppBar(
                   title: const Text('FirstPage'),
                 ),
-                body: const Center(child: Text('Hello'))),
-          )));
+                body: const Center(child: Text('Hello World!!!!!!'))),
+          ));
 }
- */
-
